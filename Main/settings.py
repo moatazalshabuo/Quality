f2b9c7f92061a76c5dd59d0f3b3a4c1887204c70
Muetaz Alsahbu <muetaz.alsahbu@zallaf.com>
2024-03-30 11:50:37 +0200

    new edit

--- Main/settings.py
+++ Main/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Quntity',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '4'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
